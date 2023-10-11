ba5e56f096ff80ea8daba2e21cab478722754b35
HRVNoa <hervieu.noa@gmail.com>
2023-10-11 11:39:25 +0200

    Commit qui n'a pas marcher

--- src/main/java/bts/sio/api/controller/Epreuve_AthleteController.java
+++ src/main/java/bts/sio/api/controller/Epreuve_AthleteController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 @RestController
 public class Epreuve_AthleteController {
@@ -22,43 +23,52 @@ public class Epreuve_AthleteController {
 
 
     @GetMapping("/epreuve_athlete/{id}")
-    public ResponseEntity<Epreuve_Athlete> getEpreuve_Athlete(@PathVariable("id") final Long id) {
-        Optional<Epreuve_Athlete> epreuveAthlete = epreuveAthleteService.getEpreuve_Athlete(id);
-        return epreuveAthlete.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<List<Epreuve_Athlete>> getEpreuve_Athlete(@PathVariable("id") final Long id) {
+        List<Epreuve_Athlete> epreuveAthletes = epreuveAthleteService.getEpreuve_Athlete(id);
+        if (!epreuveAthletes.isEmpty()) {
+            return ResponseEntity.ok(epreuveAthletes);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
     @GetMapping("/epreuves_athletes")
     public Iterable<Epreuve_Athlete> getEpreuves_Athletes() { return epreuveAthleteService.getEpreuves_Athletes();}
 
     @PutMapping("/epreuve_athlete/{id}")
-    public Epreuve_Athlete updateEpreuve_Athlete(@PathVariable("id") final Long id, @RequestBody Epreuve_Athlete epreuveAthlete) {
-        Optional<Epreuve_Athlete> e = epreuveAthleteService.getEpreuve_Athlete(id);
-        if(e.isPresent()) {
-            Epreuve_Athlete currentEpreuve_Athlete = e.get();
+    public ResponseEntity<Epreuve_Athlete> updateEpreuve_Athlete(@PathVariable("id") final Long id, @RequestBody Epreuve_Athlete epreuveAthlete) {
+        List<Epreuve_Athlete> epreuveAthletes = epreuveAthleteService.getEpreuve_Athlete(id);
 
+        if (!epreuveAthletes.isEmpty()) {
+            // Vous pouvez choisir de prendre le premier résultat ou mettre en œuvre une logique de gestion des multiples résultats.
+            // Ici, nous prenons simplement le premier résultat.
+            Epreuve_Athlete currentEpreuve_Athlete = epreuveAthletes.get(0);
+
+            // Mettez à jour les propriétés de l'objet actuel avec les nouvelles valeurs.
             Athlete athlete_id = epreuveAthlete.getAthlete();
-            if(athlete_id != null){
+            if (athlete_id != null) {
                 currentEpreuve_Athlete.setAthlete(athlete_id);
             }
 
             Epreuve epreuve_id = epreuveAthlete.getEpreuve();
-            if(epreuve_id != null){
+            if (epreuve_id != null) {
                 currentEpreuve_Athlete.setEpreuve(epreuve_id);
             }
 
             Long place = epreuveAthlete.getPlace();
-            if(place != null) {
+            if (place != null) {
                 currentEpreuve_Athlete.setPlace(place);
             }
 
             LocalDate dateResultat = epreuveAthlete.getDateResultat();
-            if(dateResultat != null){
+            if (dateResultat != null) {
                 currentEpreuve_Athlete.setDateResultat(dateResultat);
             }
 
-            epreuveAthleteService.saveEpreuve_Athlete(currentEpreuve_Athlete);
-            return currentEpreuve_Athlete;
+            Epreuve_Athlete updatedEpreuve_Athlete = epreuveAthleteService.saveEpreuve_Athlete(currentEpreuve_Athlete);
+
+            return ResponseEntity.ok(updatedEpreuve_Athlete);
         } else {
-            return null;
+            return ResponseEntity.notFound().build();
         }
     }
     @DeleteMapping("/epreuve_Athlete/{id}")
--- src/main/java/bts/sio/api/repository/Epreuve_AthleteRepository.java
+++ src/main/java/bts/sio/api/repository/Epreuve_AthleteRepository.java
@@ -4,11 +4,11 @@ import bts.sio.api.model.Epreuve_Athlete;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface Epreuve_AthleteRepository extends CrudRepository<Epreuve_Athlete, Long> {
 
-    Optional<Epreuve_Athlete> findByEpreuve_Id(final Long id);
+    List<Epreuve_Athlete> findByEpreuve_Id(final Long id);
 
 }
